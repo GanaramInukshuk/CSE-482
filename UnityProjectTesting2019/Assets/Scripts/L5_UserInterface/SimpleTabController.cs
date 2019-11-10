66ddf850ece7bb83c8443c683e57856848dcc884
GanaramInukshuk <GDO29ghhh@gmail.com>
2019-11-10 01:52:32 -0800

    Experimented with a tab system and pushing the (successful) results
    I added a tab system where clicking a tab switches to some different UI panel; this will be useful for showing more stuff on the screen and organizing the different controls (IE, separating the zoning and civic simulators).
    
    This, however, means that a few things are out of place at the moment as things get shuffled around.

---
+++ UnityProjectTesting2019/Assets/Scripts/L5_UserInterface/SimpleTabController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This script is for adding a simple tabbed menu system to the UI
+// All this needs is an array of buttons and an array of tabbed pages corresponding
+// to the buttons
+
+public class SimpleTabController : MonoBehaviour {
+
+    public Button[] tabButtons;
+    public Image [] tabPages;
+
+    // The start function sets the first page to be displayed by default
+    private void Start() {
+        for (int i = 1; i < tabButtons.Length; i++) {
+            tabPages[i].gameObject.SetActive(false);
+        }
+
+        // Set up listeners to each button
+        for (int i = 0; i < tabButtons.Length; i++) {
+            int j = i;
+            tabButtons[i].onClick.AddListener(() => SwapPages(j));
+        }
+    }
+
+    public void SwapPages(int pageNumber) {
+        for (int i = 0; i < tabButtons.Length; i++) {
+            if (pageNumber == i) tabPages[i].gameObject.SetActive(true);
+            else tabPages[i].gameObject.SetActive(false);
+        }
+    }
+}
